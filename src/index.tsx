bedba456ad7d1565b94e114d354acdd5044a6fe0
Ashley Davis <ashley@codecapers.com.au>
2022-07-24 18:48:03 +1000

    Converted this example to Parcel 2.

--- src/index.tsx
+++ src/index.tsx
@@ -1,24 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Layout } from "./pages/layout";
-import { Page1 } from "./pages/page-1";
-import { Page2 } from "./pages/page-2";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
 
-class App extends React.Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route path="page-1" element={<Page1 />} />
-                        <Route path="page-2" element={<Page2 />} />
-                        <Route path="/" element={<Navigate replace to="/page-1" />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        );                
-    }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root")!);
+root.render(<App />);
